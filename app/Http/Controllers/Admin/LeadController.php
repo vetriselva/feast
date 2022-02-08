b93ea22293a2af6c0592af6e326e944673fde2b0
vetriselva <selvamvetri27@gmail.com>
2022-02-08 22:10:39 +0530

    pdf list changes

--- app/Http/Controllers/Admin/LeadController.php
+++ app/Http/Controllers/Admin/LeadController.php
@@ -157,8 +157,8 @@ class LeadController extends Controller
         $hotelDetails = Collect($data->HotalsDeatils)->groupBy('HotelOptionNumber');
         $costDeatils = Collect($data->CostDeatils)->groupBy('optionNumber');
         $paymentPolicies = PaymentPolicy::find( json_decode($data->payment_poly));
-        $refundPolicies = RefoundPolicy::find( json_decode($data->refund_poly));
-        $cancelPolicies = CanclePolicy::find( json_decode($data->cancel_poly));
+        $refundPolicies = RefoundPolicy::find( json_decode($data->refound_poly));
+        $cancelPolicies = CanclePolicy::find( json_decode($data->cancle_poly));
         $packInclusions = PackageInclusions::find( json_decode($data->pack_includs));
         $packExclusions = PackageExclusions::find( json_decode($data->pack_excluds));
         return view("admin.lead.show-lead",compact('data','hotelDetails', 'costDeatils','paymentPolicies', 'refundPolicies','cancelPolicies','packInclusions','packExclusions'));
